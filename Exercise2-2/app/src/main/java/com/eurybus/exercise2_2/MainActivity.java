f50451488d19a004ba2f6a33c20aa826ceea1396
Eurybus <Wraitholyth@gmail.com>
2016-09-11 17:10:27 +0300

    Finished Exercise 02-2

--- Exercise2-2/app/src/main/java/com/eurybus/exercise2_2/MainActivity.java
+++ Exercise2-2/app/src/main/java/com/eurybus/exercise2_2/MainActivity.java
@@ -2,10 +2,16 @@ package com.eurybus.exercise2_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView actv;
+    TextView pwField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //AutoCompleteTextView
-        AutoCompleteTextView actv = (AutoCompleteTextView) findViewById(R.id.login_field);
+        actv = (AutoCompleteTextView) findViewById(R.id.login_field);
         ArrayAdapter<String> aa = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,
                 new String[]{
                         "Pasi","Arto","Sami","Makke","Juhani","Essi"
                 });
         actv.setAdapter(aa);
+
+        //PasswordField
+        pwField = (TextView) findViewById(R.id.pw_field);
+    }
+
+    public void loginButtonPressed(View view){
+        Toast.makeText(MainActivity.this,actv.getText() + " " + pwField.getText(),Toast.LENGTH_SHORT).show();
     }
 }
